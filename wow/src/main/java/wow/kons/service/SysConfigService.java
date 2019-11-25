7a2896220404589a0e050a9d4510cd52c01a0a9e
jerry <jerry975@126.com>
2019-11-25 20:55:34 +0800

    增加菜单层级信息API

--- wow/src/main/java/wow/kons/service/SysConfigService.java
+++ wow/src/main/java/wow/kons/service/SysConfigService.java
@@ -4,9 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import wow.kons.bean.Menu;
 import wow.kons.dao.MenuDao;
-import wow.kons.model.MenuJsonModel;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
